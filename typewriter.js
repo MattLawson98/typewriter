3da622a98e134ad5d167f538a075c72f55672d03
MattLawson98 <matthewlawson50@gmail.com>
2022-03-15 16:36:24 -0700

    This function makes a phrase print out like a typewriter

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let x = 1000;
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char);
+  }, x)
+  x += 50;
+}
+setTimeout(() => {
+  console.log();
+},x);
